42907a7156d5490ac255b2f502b30b75754cb098
jaber <ahamedjaber8@gmail.com>
2025-07-17 16:13:00 +0600

    feat(Dependency): update dependency and added hilt, ksp, NavigationCompose, retrofit, moshi, kotlinxSerializationCore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.plugin)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("kotlin-parcelize")
+}
+
+
+ksp {
+    arg("org.gradle.annotation.processing.options", "-Xlint:deprecation")
+    // You might have other KSP arguments here
+    // arg("hilt.some.ksp.option", "value")
 }
 
 android {
@@ -49,6 +60,24 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.lottie.compose.v660)
+    implementation(libs.navigation.compose)
+    implementation(libs.androidx.material3.icons)
+    implementation(libs.lottie.compose)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
+    implementation(libs.logging.interceptor)
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+    implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.jetbrains.serialization)
+    implementation(libs.jetbrains.serialization.json)
+    implementation(libs.play.service.location)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
